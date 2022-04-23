ad784209138cf162de76ff45567106ce880fcb71
Devika Prashad <devikaprashad@Devikas-MacBook-Air.local>
2022-04-22 21:07:38 -0400

    Add another env variable to report function

--- src/com/yorku/DemoLibrary.groovy
+++ src/com/yorku/DemoLibrary.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 package com.yorku
 
-def outputReport(buildUser){
+def outputReport(){
         echo "Hello, this is your report ${env.BUILD_TAG} "
-        println ("User is " + buildUser)
+        println ("User is " + ${env.NODE_LABELS})
     }

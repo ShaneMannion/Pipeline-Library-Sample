427b055eef5985be45d61418533a32bc3fea3319
Devika Prashad <devikaprashad@Devikas-Air.hitronhub.home>
2022-04-22 22:14:05 -0400

    Additional report formatting

--- src/com/yorku/DemoLibrary.groovy
+++ src/com/yorku/DemoLibrary.groovy
@@ -2,12 +2,12 @@
 package com.yorku
 
 def outputReport(){
-        def title = "\t\t\t\t" + "Jenkins build report for " + env.BUILD_TAG + "\n"
+        def title = "\t\t" + "Jenkins build report for " + env.BUILD_TAG + "\n"
         def buildDetails = "  Jenkins job '" + env.JOB_BASE_NAME + "' was executed by user:" + env.USER + ".\n"
         def nodeDetails = "  The job executed on node " + env.NODE_NAME + ".\n" 
         def workspaceDetails = "  The workspace was " + env.WORKSPACE + ".\n"
         def serverDetails = "  Jenkins login URL " + env.HUDSON_URL + ".\n"
         def border = "***********************************************************" + "\n"
 
-        println (border + border + title + border + buildDetails + nodeDetails + serverDetails + border + border)
+        println (border + border + title + border + buildDetails + nodeDetails + workspaceDetails + serverDetails + border + border)
     }

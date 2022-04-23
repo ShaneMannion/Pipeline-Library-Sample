9b07da49ec9274f165b455f47e26ddbecfeec970
Devika Prashad <devikaprashad@Devikas-Air.hitronhub.home>
2022-04-23 07:44:36 -0400

    removed job notes

--- src/com/yorku/DemoLibrary.groovy
+++ src/com/yorku/DemoLibrary.groovy
@@ -1,19 +1,13 @@
 #!/usr/bin/env groovy
 package com.yorku
 
-def outputReport(JOB_NOTES){
+def outputReport(){
         def title = "\t" + "Jenkins build report for " + env.BUILD_TAG + "\n"
         def buildDetails = "  Jenkins job '" + env.JOB_BASE_NAME + "' was executed by user:" + env.USER + ".\n"
         def nodeDetails = "  The job executed on node " + env.NODE_NAME + ".\n" 
         def workspaceDetails = "  The workspace was " + env.WORKSPACE + ".\n"
         def serverDetails = "  Jenkins login URL " + env.HUDSON_URL + ".\n"
         def border = "***********************************************************" + "\n"
-        if (JOB_NOTES != null){
-            def jobNotes = " Notes for this job: '" + JOB_NOTES + "'.\n"
-        }
-        else{
-            def jobNotes = ""
-        }
-        
-        println (border + border + title + border + buildDetails + nodeDetails + workspaceDetails + serverDetails + jobNotes + border + border)
+
+        println (border + border + title + border + buildDetails + nodeDetails + workspaceDetails + serverDetails + border + border)
     }

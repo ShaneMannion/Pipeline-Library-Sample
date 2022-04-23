dcffa885f8ac25e5c6f0234e1c2488b2c8c96f33
Devika Prashad <devikaprashad@Devikas-Air.hitronhub.home>
2022-04-22 22:14:55 -0400

    Fixing tabs on report

--- src/com/yorku/DemoLibrary.groovy
+++ src/com/yorku/DemoLibrary.groovy
@@ -2,7 +2,7 @@
 package com.yorku
 
 def outputReport(){
-        def title = "\t\t" + "Jenkins build report for " + env.BUILD_TAG + "\n"
+        def title = "\t" + "Jenkins build report for " + env.BUILD_TAG + "\n"
         def buildDetails = "  Jenkins job '" + env.JOB_BASE_NAME + "' was executed by user:" + env.USER + ".\n"
         def nodeDetails = "  The job executed on node " + env.NODE_NAME + ".\n" 
         def workspaceDetails = "  The workspace was " + env.WORKSPACE + ".\n"

75d04506e0b5dadeede9a547272f0da4c6687bd2
Devika Prashad <devikaprashad@Devikas-MacBook-Air.local>
2022-04-22 20:30:59 -0400

    change output

--- src/com/yorku/DemoLibrary.groovy
+++ src/com/yorku/DemoLibrary.groovy
@@ -1,10 +1,6 @@
 #!/usr/bin/env groovy
 package com.yorku
 
-class DemoLibrary {
-
-    def outputReport(){
-        println("Hello, this is your report")
+def outputReport(){
+        eho "Hello, this is your report"
     }
-
-}
